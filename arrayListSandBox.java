9d1a1ea5ee668d95ce0b047e023ae70317023674
HermanoJose <joseph.travis.byui@gmail.com>
2016-09-24 13:49:17 -0600

    Add files via upload

---
+++ arrayListSandBox.java
@@ -0,0 +1,140 @@
+import jdk.nashorn.internal.runtime.ECMAException;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Joseph on 9/15/16.
+ */
+public class arrayListSandBox {
+
+    public static void main(String[] params) {
+        //sandbox constructor for ArrayList
+
+        //Happy Path - constructing an ArrayList of a String type
+        ArrayList<Integer> intArray = new ArrayList<Integer>();
+
+        intArray.add(15);
+        intArray.add(30);
+        intArray.add(45);
+        intArray.add(60);
+
+        System.out.println(intArray);
+        //result - the array is created and prints [] because it is unpopulated.
+
+        //Nasty Path - add null, what will happen if I add null. In C it will end the array because a lot of arrays
+        //are null terminated
+        intArray.add(null);
+        intArray.add(75);
+        System.out.println(intArray + "\n");
+        //It just continues through the array. It doesn't seem to care that there is a null in there. Not good if we
+        // are expecting a value in our program
+
+        //Nasty Path - adding a number to an index that is not in our array yet, will it then add it to the array if
+        // the array gets bigger or will it resize the array or will we have a problem?
+        try {
+            intArray.add(75, null);
+        } catch (Exception e) {
+            System.out.println("You get an OutOfBounds Exception! You can't do this at all!\n");
+        }
+        //You get an error. and it won't let you do this. Its just too big, you have to insert items into an index
+        //that is already there.
+
+        //Nasty Path - inserting something into a negative index. Why would you want to do this? I don't know
+        try {
+            intArray.add(-1, -1);
+        } catch (Exception e) {
+            System.out.println("This can't be done either! You don't want negative array indexes!\n");
+        }
+        //When I try to do this we get an exception saying that we cannot do this. Arrays go up, not down, you cannot
+        //Have a negative index.
+
+        //Happy Path - get method
+        System.out.println(intArray.get(0));
+
+        //Nasty Path - what happens if I try to get a negative index? will it convert it to positive?
+        try {
+            System.out.println(intArray.get(-1));
+        } catch (Exception e) {
+            System.out.println("You can't do this! It looks for this spot in the array! You only have items in " +
+                    "0, 1, 2, 3, 4, and 5\n");
+        }
+        //You can't have a negative index, and this throws our exception.
+
+        //Happy Path - adding inside the array to get an index value
+        System.out.println(intArray.get(1 + 2) + "\n");
+
+        //Nasty Path - what happens if I try to add something at the min value and then subtract one from it?
+        try {
+            intArray.add(Integer.MIN_VALUE);
+            System.out.println(intArray.get(6) - 1 + "\n");
+        } catch (Exception e) {
+            System.out.println("Anything past min value wraps to the max value!\n");
+        }
+        //It wraps! when you subtract one from -2147483647 it becomes 2147483647! Same thing happens if I go in reverse
+
+        //Nasty path - what happens when I try dividing a result by 0?
+        try {
+            System.out.println(intArray.get(3) / 0);
+        } catch (Exception e) {
+            System.out.println("You can't divide by zero\n");
+        }
+
+        //index too big
+        // index is negative, biggest or smallest possible integer + adding it. divide by 0 double.min / something
+
+        //Happy Path - add all
+        ArrayList<Integer> intArray2 = new ArrayList<>();
+        intArray2.add(0);
+        intArray2.add(1);
+        intArray2.add(2);
+        intArray2.add(3);
+
+        intArray.addAll(intArray2);
+
+        System.out.println(intArray);
+
+        //Nasty Path - Merging an empty collection
+        ArrayList<Integer> emptyArray = new ArrayList<>();
+
+        try {
+            intArray.addAll(emptyArray);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(intArray);
+        //Doesnt error out, doesn't give you a warning, just does it, which might not be something that you want to happen
+
+
+        //Happy Path - remove
+        intArray.remove(0);
+        System.out.println(intArray);
+
+        //Nasty Path - remove an element based off of value instead of position
+        try {
+            intArray.remove(75);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        //GOes to the index no matter what, you cannot use values. You get an out of bounds exception
+
+
+        //Nasty Path - Try to remove a null value
+        try {
+            intArray.remove(null);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(intArray);
+        //It goes through, no warnings, no nothing. It removes both the null value and the null reference!
+
+
+    }
+}
---
+++ hashMapSandBox.java
@@ -0,0 +1,106 @@
+import java.util.HashMap;
+
+/**
+ * Created by Joseph on 9/20/16.
+ */
+public class hashMapSandBox {
+    public static void main (String[] params) {
+
+        //HashMap Happy Path - Create a HashMap and add some items to it
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("A", "Travis");
+        hashMap.put("B", "Sarah");
+        hashMap.put("C", "Anthony");
+        hashMap.put("D", "Melody");
+
+        for (String key : hashMap.keySet()) {
+            System.out.println(key + ": " + hashMap.get(key));
+        }
+
+
+        //Nasty path - outputting hashMap - if we want to output hashMap we need to use the key to get specific elements
+        // or else it will just spit out the entire map instead of just what you want. You must also use the key to
+        // get the value or else you will just get a null. Remember that a value and a key are not the same thing.
+        System.out.println(hashMap);
+        try {
+            System.out.println(hashMap.get("A"));
+            System.out.println(hashMap.get("Travis"));
+        }
+        catch (Exception e){
+            System.out.println("You have to use a key value pair to output your hashMap value!");
+        }
+
+        //Happy Path - remove
+        hashMap.remove("D");
+        System.out.println(hashMap);
+
+        //Nasty Path - can I delete an item based off its value rather than its key?
+        try {
+            hashMap.remove("Travis");
+            for (String key : hashMap.keySet()) {
+                System.out.println(key + ": " + hashMap.get(key));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Travis is still there! There isn't even an exception thrown!");
+        }
+        //When you use the remove method, you have to specify a key. If you try to use a value or you use a key that
+        //doesn't exist you will not get an error, you will not get an exception, or any message. Simply, the computer
+        //notices that there is no key like the one you typed in and thus it must have been removed. You have to be careful
+        //because there isn't anything to tell you that you succeeded with this command. If you have automated
+        //this removal, you will need to make sure the key is correctly passed to the remove method.
+
+        //Nasty Path - what happens if I try to put a null into hashMap as a key or a value? Will it allow it or will
+        //it spit out an exception?
+        try {
+            hashMap.put("E", null);
+        }
+        catch (Exception e) {
+            System.out.println("Will this work?");
+        }
+
+        try {
+            hashMap.put(null, "F");
+        }
+        catch (Exception e) {
+            System.out.println("Will that work?");
+        }
+
+        System.out.println(hashMap.get(null));
+        System.out.println(hashMap.get("E"));
+        //Both of these work! Be careful, because if you store a null key then you need to make sure not override it.
+        //If it is in an automated function you might have a hard time getting a null value to produce that result, it might
+        //be useful in some error handling situations though. Also be careful, that if a program produces a null value
+        // for a value, then it will store it without an error, which may go unnoticed and be unintentional, and cause
+        //greater problems later on.
+
+        //Happy Path - create an integer hashmap with some values put in it.
+        HashMap<Integer, Integer> integerMap = new HashMap<>();
+        integerMap.put(0, 0);
+        integerMap.put(1, 1);
+        integerMap.put(2, 2);
+        integerMap.put(3, 3);
+
+        //Nasty Path - lets try a null value as a key and as a value and see what happens
+        integerMap.put(null, 4);
+        integerMap.put(4, null);
+
+        for(Integer key : integerMap.keySet()) {
+            System.out.println(key + ": " + integerMap.get(key));
+        }
+        System.out.println("\n");
+        //It outputs everything! even the null key and null value, it accepts them without errors even though it
+        //probably shouldn't. At least we can use this as a way to handle errors or something like that.
+
+        //Nasty Path - What happens if I use a negative key?
+        integerMap.put(-1, -1);
+
+        for(Integer key : integerMap.keySet()) {
+            System.out.println(key + ": " + integerMap.get(key));
+        }
+
+        System.out.println('\n');
+        System.out.println(integerMap);
+
+    }
+}

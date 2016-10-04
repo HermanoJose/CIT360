9f6bfad5081243d21314a244a3865d494d0328e8
aymelvel <ayancyvelarde@gmail.com>
2016-10-04 12:18:03 -0600

    Add files via upload

---
+++ treeSetSandBox.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created by Ayancy on 10/4/2016.
+ */
+public class treeSetSandBox {public static void main(String[] args) {
+
+    Set<Integer> a = new TreeSet<Integer>(Arrays.asList(new Integer[]{0, 2, 4, 5, 6, 8, 10}));
+    Set<Integer> b = new TreeSet<Integer>(Arrays.asList(new Integer[]{5, 6, 7, 8, 9, 10}));
+
+    //happy path create union
+    Set<Integer> c = new TreeSet<Integer>(a);
+    c.addAll(b);
+    System.out.println(c);
+
+    //Nasty path add a
+    try {
+        Set.add(null);
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+
+
+    //happy path create intersection
+    Set<Integer> d = new TreeSet<Integer>(a);
+    d.retainAll(b);
+    System.out.println(d);
+
+
+    //happy path create difference
+    Set<Integer> e = new TreeSet<Integer>(a);
+    e.removeAll(b);
+    System.out.println(e);
+
+
+
+}
+    }

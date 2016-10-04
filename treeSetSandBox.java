984cf0e56e8bebeedddddebee4bd048a93c0a0d1
aymelvel <ayancyvelarde@gmail.com>
2016-10-04 12:35:33 -0600

    Update treeSetSandBox.java

--- treeSetSandBox.java
+++ treeSetSandBox.java
@@ -3,18 +3,21 @@ import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * Created by Ayancy on 10/4/2016.  no nasty paths added yet
+ * Created by Ayancy on 10/4/2016.
  */
 public class treeSetSandBox {public static void main(String[] args) {
 
     Set<Integer> a = new TreeSet<Integer>(Arrays.asList(new Integer[]{0, 2, 4, 5, 6, 8, 10}));
     Set<Integer> b = new TreeSet<Integer>(Arrays.asList(new Integer[]{5, 6, 7, 8, 9, 10}));
-
+    //nasty path adding a null
+    Set<Integer> f = new TreeSet<Integer>(Arrays.asList(new Integer[]{5, 6, 7, 8, 9, null}));
+    //nasty path adding an empty set
+    Set<Integer> g = new TreeSet<Integer>(Arrays.asList(new Integer[]{}));
+    
     //happy path create union
     Set<Integer> c = new TreeSet<Integer>(a);
     c.addAll(b);
     System.out.println(c);
-    
 
     //happy path create intersection
     Set<Integer> d = new TreeSet<Integer>(a);

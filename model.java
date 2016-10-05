8f9111831b3d162d437a2c3d401451f6e47450b7
cschunsiu <cs_chunsiu@yahoo.com.hk>
2016-10-04 18:00:58 -0600

    Add files via upload
    MVC

---
+++ model.java
@@ -0,0 +1,25 @@
+package MVC;
+
+/**
+ * Created by cs_ch on 10/2/2016.
+ */
+public class model {
+    private int legs;
+    private String name;
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ view.java
@@ -0,0 +1,11 @@
+package MVC;
+
+/**
+ * Created by cs_ch on 10/2/2016.
+ */
+public class view {
+    public void animalCout(String name, int legs){
+        System.out.println("Animal: "+ name);
+        System.out.println("legs count: " + legs);
+    }
+}
